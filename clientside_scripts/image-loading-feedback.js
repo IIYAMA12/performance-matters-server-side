03bc16192fa1bdc6e4a1fad472592f94338c2068
IIYAMA12 <jorik@253.146.028.145.hva.nl>
2018-03-23 12:33:12 +0100

    update image-loading-feedback

--- clientside_scripts/image-loading-feedback.js
+++ clientside_scripts/image-loading-feedback.js
@@ -1,8 +1,21 @@
 const imageLoadingFeedback = {
-    isLoaded () {
-        console.log("loaded");
+    onImageLoad (e) {
+        this.classList.add("loaded");
     }
 };
 
+
+document.getElementsByTagName("body")[0].classList.add("js-available");
+const images = document.getElementById("map-information").getElementsByTagName("img");
+for (let index = 0; index < images.length; index++) {
+    const image = images[index];
+    if (!image.complete) {
+        console.log("add listener");
+        
+        image.addEventListener("load", imageLoadingFeedback.onImageLoad);
+    } else {
+        image.classList.add("loaded");
+    }
+}
 
 module.exports = imageLoadingFeedback;
--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -1,10 +1,23 @@
 (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 const imageLoadingFeedback = {
-    isLoaded () {
-        console.log("loaded");
+    onImageLoad (e) {
+        this.classList.add("loaded");
     }
 };
 
+
+document.getElementsByTagName("body")[0].classList.add("js-available");
+const images = document.getElementById("map-information").getElementsByTagName("img");
+for (let index = 0; index < images.length; index++) {
+    const image = images[index];
+    if (!image.complete) {
+        console.log("add listener");
+        
+        image.addEventListener("load", imageLoadingFeedback.onImageLoad);
+    } else {
+        image.classList.add("loaded");
+    }
+}
 
 module.exports = imageLoadingFeedback;
 },{}],2:[function(require,module,exports){
